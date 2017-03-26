4b25996be6097a3842de70685e78fccb6766c5d1
athan <athanasios.moutsioulis@gmail.com>
2017-03-26 19:16:44 +0100

    v1_change

--- app/src/main/java/com/mts/sakis/gitporjecttest/MainActivity.java
+++ app/src/main/java/com/mts/sakis/gitporjecttest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String strTest = "Git test";
+        String strTest = "Git";
     }
 }
