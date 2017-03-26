1e2d98ad57b5fe9dc06def9661d0e45f3b184768
athan <athanasios.moutsioulis@gmail.com>
2017-03-26 19:59:01 +0100

    v1_second commit

--- app/src/main/java/com/mts/sakis/gitporjecttest/MainActivity.java
+++ app/src/main/java/com/mts/sakis/gitporjecttest/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String strTest = "Git";
+        String strTest = "Git stash change";
         String strTest2 = "sa";
     }
 }
